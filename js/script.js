65eea052be72171a0732264e98bc26c23f84d359
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-08-16 10:15:17 +0200

    mise a jour du portfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Bienvenue sur mon portfolio statique !");
+// Mettre à jour l'année automatiquement
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Copier email au clic droit
+const emailBtns = document.querySelectorAll('a[href^="mailto:"]');
+for(const btn of emailBtns){
+  btn.addEventListener('contextmenu', e => {
+    e.preventDefault();
+    navigator.clipboard.writeText('ousmanesarrd@gmail.com').then(()=>{
+      btn.textContent = '📋 Email copié !';
+      setTimeout(()=>{ btn.textContent = '✉️ ousmanesarrd@gmail.com'; }, 1500);
+    });
+  });
+}
